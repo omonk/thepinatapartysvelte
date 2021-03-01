e157e4cf1cb0963a69a150dd8aff62ff6d0dad02
Ollie Monk <ollie@olliemonk.com>
2021-03-01 07:53:30 +0000

    Stop Ross being a dick

--- api/invites/get.js
+++ api/invites/get.js
@@ -1,8 +1,13 @@
 const { getInvites } = require("../../src/api");
 
 module.exports = async (req, res) => {
-  const invites = await getInvites();
+  if (req.headers["x-key"] === "fuck-off-ross") {
+    const invites = await getInvites();
 
-  res.json({ inviteKeys: Object.keys(invites) });
-  res.send();
+    res.json({ inviteKeys: Object.keys(invites) });
+    res.send();
+  } else {
+    res.status(403);
+    res.send("Go away nerd");
+  }
 };
